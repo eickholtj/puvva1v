be166f88c892e48c0cfe192ac34e216eff053899
puvva1v <puvva1v@cmich.edu>
2018-02-27 12:00:39 -0500

    Ebook

---
+++ ebook/src/main/java/com/cmich/ebook/EbookApplication.java
@@ -0,0 +1,14 @@
+package com.cmich.ebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookApplication.class, args);
+
+	}
+}
+
