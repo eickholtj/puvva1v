d76cd807bde08430f647754330d6f11f7db34f9d
puvva1v <36869279+puvva1v@users.noreply.github.com>
2018-04-28 15:23:33 -0400

    Update bootstrap-dev.sh

--- scripts/bootstrap-dev.sh
+++ scripts/bootstrap-dev.sh
@@ -2,7 +2,6 @@
 
 BASEPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-python $BASEPATH/annotations.py
 python $BASEPATH/book_chapters.py
 python $BASEPATH/book_chapters_sections.py
 python $BASEPATH/books.py
